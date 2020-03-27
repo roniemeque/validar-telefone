0d5834d1932a97be6ef590b159e65fe79a1c37bd
Ronie <roniemeque@icloud.com>
2020-03-27 16:41:50 -0300

    sanitizing input to be transformed

---
+++ __tests__/helpers.test.ts
@@ -0,0 +1,18 @@
+import { cleanAndNormalizeInput } from "../src/helpers";
+
+describe("CleanAndNormalizeInput tests", () => {
+  test("should throw error when input is not a string or number", () => {
+    // @ts-ignore
+    expect(() => cleanAndNormalizeInput({})).toThrowError(
+      "O input precisa ser string ou numérico"
+    );
+  });
+
+  test("should return a valid string when input is numeric", () => {
+    expect(cleanAndNormalizeInput(9999)).toBe("9999");
+  });
+
+  test("should return a string without common phone special chars", () => {
+    expect(cleanAndNormalizeInput("+55 (99) 91234-4321")).toBe("5599912344321");
+  });
+});
---
+++ src/helpers.ts
@@ -0,0 +1,11 @@
+export const cleanAndNormalizeInput = (phone: string | number): string => {
+  if (typeof phone === "number") {
+    return `${phone}`;
+  }
+
+  if (typeof phone === "string") {
+    return phone.replace(/\D/g, "");
+  }
+
+  throw new Error("O input precisa ser string ou numérico");
+};
